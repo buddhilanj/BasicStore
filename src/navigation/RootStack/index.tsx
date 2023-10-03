991eb407c24a2ccb88fcae4b4ca5bc8d664715d8
buddhilanj <buddhilanj@gmail.com>
2023-10-03 16:58:46 +1100

    splash screen wait till data load

--- src/navigation/RootStack/index.tsx
+++ src/navigation/RootStack/index.tsx
@@ -1,17 +1,17 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 
-import SCREENS from '@constants/screens';
 import AppStack from '@navigation/AppStack';
+import { RootStackParamList } from '@navigation/types';
 import SplashScreen from '@screens/splash';
 
-const Root = createNativeStackNavigator();
+const Root = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootStack() {
   return (
     <Root.Navigator screenOptions={{ headerShown: false }}>
-      <Root.Screen name={SCREENS.SPLASH} component={SplashScreen} />
-      <Root.Screen name={SCREENS.APP._} component={AppStack} />
+      <Root.Screen name="SplashScreen" component={SplashScreen} />
+      <Root.Screen name="AppStack" component={AppStack} />
     </Root.Navigator>
   );
 }
--- src/screens/splash/index.tsx
+++ src/screens/splash/index.tsx
@@ -1,9 +1,21 @@
+import { useNavigation } from '@react-navigation/native';
 import { Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useAppSelector } from '@hooks/reduxHooks';
+import { SplashScreenNavigationProp } from '@navigation/types';
 
 import styles from './style';
 
 export default function SplashScreen() {
+  const isProductsLoaded = useAppSelector(state => state.products.status === 'succeeded');
+  const navigation = useNavigation<SplashScreenNavigationProp>();
+  useEffect(() => {
+    if (isProductsLoaded) {
+      navigation.navigate('AppStack');
+    }
+  }, [isProductsLoaded]);
+
   return (
     <View style={styles.container}>
       <Text>Splash Screen</Text>
